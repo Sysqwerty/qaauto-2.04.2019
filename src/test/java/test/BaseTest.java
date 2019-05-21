04184f2a31e6c613215ffbdccc1a794c9f6942a3
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2019-05-21 13:09:33 +0300

    Lesson 11 XML Suites @Parameter 'browserName'

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,24 +1,38 @@
 package test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import page.LoginPage;
 
 abstract class BaseTest {
   protected WebDriver driver;
   LoginPage loginPage;
 
+  @Parameters("browserName")
   @BeforeMethod
-  void beforeMethod() {
+  void beforeMethod(@Optional("chrome") String browserName) throws Exception {
     System.out.println("1st before method: Open Chrome browser on Login page");
     //driver initialization and open start page
-    driver = new ChromeDriver();
+    if (browserName.toLowerCase().equals("chrome")) {
+      WebDriverManager.chromedriver().setup();
+      driver = new ChromeDriver();
+    } else if (browserName.toLowerCase().equals("firefox")) {
+      WebDriverManager.firefoxdriver().setup();
+      driver = new FirefoxDriver();
+    } else {
+      throw new Exception("Unsupported 'browserName'");
+    }
+
+
     driver.manage().deleteAllCookies();
     driver.get("https://www.linkedin.com/");
     driver.manage().window().maximize();
-
     loginPage = new LoginPage(driver);
   }
 
